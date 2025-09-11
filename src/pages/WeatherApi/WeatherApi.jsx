6d8237b77bf8f7e688c221b895cb13b9730a322f
WonYoung <error071312@gmail.com>
2025-09-12 02:03:39 +0900

    날씨 한글 변환

--- src/pages/WeatherApi/WeatherApi.jsx
+++ src/pages/WeatherApi/WeatherApi.jsx
@@ -16,7 +16,8 @@ function WeatherApi() {
     const fetchWeather = async (lat, lng) => {
       try {
         // API 요청 URL 생성
-        const url = `${API_BASE_URL}?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}`;
+        const url = `${API_BASE_URL}?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}&lang=kr&units=metric`;
+        // 한글, 섭씨
 
         // get요청 날리기
         const response = await axios.get(url);
@@ -55,10 +56,6 @@ function WeatherApi() {
     }
   }, []);
 
-  // Kelvin -> 섭씨 변환 함수
-  // temp: 295.35 -> 22.2'C
-  const kelvinToCelcius = (k) => (k - 273.15).toFixed(1);
-
   const weatherLabels = {
     temp: "현재 기온",
     feels_like: "체감 기온",
@@ -71,12 +68,12 @@ function WeatherApi() {
   return (
     <div css={s.container}>
       <h1>날씨 API TEST</h1>
-      <div>현재 위치는..</div>
+      <div>현재 위치는.. </div>
       <div>위도: {coords.lat}</div>
       <div>경도: {coords.lng}</div>
       <div>---------------------------------</div>
-      <div>현재 날씨는.. {weather?.weather[0].main}</div>
-      <div>현재 기온은.. {kelvinToCelcius(weather?.main.temp)}'C</div>
+      <div>현재 날씨는.. {weather?.weather[0].description}</div>
+      <div>현재 기온은.. {weather?.main.temp}'C</div>
       {/* For code 500 - light rain icon = "10d". See below a full list of codes
         URL is https://openweathermap.org/img/wn/10d@2x.png */}
       <img
